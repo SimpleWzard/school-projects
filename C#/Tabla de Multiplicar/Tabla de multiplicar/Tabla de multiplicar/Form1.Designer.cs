b0bbf9e452070fb30132384fdcc703a939b793e1
Jaime Hernández <59671227+SimpleWzard@users.noreply.github.com>
2021-05-17 23:07:47 -0400

    Add files via upload

---
+++ C#/Tabla de Multiplicar/Tabla de multiplicar/Tabla de multiplicar/Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace Tabla_de_multiplicar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Garamond", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12"});
+            this.comboBox1.Location = new System.Drawing.Point(73, 34);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(43, 35);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.Text = "1";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button1.Location = new System.Drawing.Point(140, 57);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(123, 29);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "MIRA LA TABLA";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label1.Font = new System.Drawing.Font("Garamond", 18F);
+            this.label1.Location = new System.Drawing.Point(84, 100);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 27);
+            this.label1.TabIndex = 2;
+            this.label1.Text = " ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label2.Font = new System.Drawing.Font("Garamond", 18F);
+            this.label2.Location = new System.Drawing.Point(84, 129);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(18, 27);
+            this.label2.TabIndex = 3;
+            this.label2.Text = " ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label3.Font = new System.Drawing.Font("Garamond", 18F);
+            this.label3.Location = new System.Drawing.Point(84, 158);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(18, 27);
+            this.label3.TabIndex = 4;
+            this.label3.Text = " ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label4.Font = new System.Drawing.Font("Garamond", 18F);
+            this.label4.Location = new System.Drawing.Point(84, 187);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(18, 27);
+            this.label4.TabIndex = 5;
+            this.label4.Text = " ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label5.Font = new System.Drawing.Font("Garamond", 18F);
+            this.label5.Location = new System.Drawing.Point(84, 216);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(18, 27);
+            this.label5.TabIndex = 6;
+            this.label5.Text = " ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label6.Font = new System.Drawing.Font("Garamond", 18F);
+            this.label6.Location = new System.Drawing.Point(84, 245);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(18, 27);
+            this.label6.TabIndex = 7;
+            this.label6.Text = " ";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label7.Font = new System.Drawing.Font("Garamond", 18F);
+            this.label7.Location = new System.Drawing.Point(84, 274);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(18, 27);
+            this.label7.TabIndex = 8;
+            this.label7.Text = " ";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label8.Font = new System.Drawing.Font("Garamond", 18F);
+            this.label8.Location = new System.Drawing.Point(84, 303);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(18, 27);
+            this.label8.TabIndex = 9;
+            this.label8.Text = " ";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label9.Font = new System.Drawing.Font("Garamond", 18F);
+            this.label9.Location = new System.Drawing.Point(84, 332);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(18, 27);
+            this.label9.TabIndex = 10;
+            this.label9.Text = " ";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label10.Font = new System.Drawing.Font("Garamond", 18F);
+            this.label10.Location = new System.Drawing.Point(81, 361);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(18, 27);
+            this.label10.TabIndex = 11;
+            this.label10.Text = " ";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label11.Font = new System.Drawing.Font("Garamond", 18F);
+            this.label11.Location = new System.Drawing.Point(81, 390);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(18, 27);
+            this.label11.TabIndex = 12;
+            this.label11.Text = " ";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label12.Font = new System.Drawing.Font("Garamond", 18F);
+            this.label12.Location = new System.Drawing.Point(81, 419);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(18, 27);
+            this.label12.TabIndex = 13;
+            this.label12.Text = " ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(275, 470);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Tabla de multiplicar";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+    }
+}
+
---
+++ C#/Tabla de Multiplicar/Tabla de multiplicar/Tabla de multiplicar/Form1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tabla_de_multiplicar { public partial class Form1 : Form { public Form1() { InitializeComponent(); } int b; 
+    private void multiplica() { string a = comboBox1.Text; switch (a) { case "1": b = 1; break; case "2": b = 2; break; case "3": b = 3; break; case "4": b = 4; break; case "5": b = 5; break; case "6": b = 6; break; case "7": b = 7; break; case "8": b = 8; break; case "9": b = 9; break; case "10": b = 10; break; case "11": b = 11; break; case "12": b = 12; break; } } private void button1_Click(object sender, EventArgs e) { multiplica(); label1.Text = "1 x " + b + " = " + (b * 1).ToString(); label2.Text = "2 x " + b + " = " + (b * 2).ToString(); label3.Text = "3 x " + b + " = " + (b * 3).ToString(); label4.Text = "4 x " + b + " = " + (b * 4).ToString(); label5.Text = "5 x " + b + " = " + (b * 5).ToString(); label6.Text = "6 x " + b + " = " + (b * 6).ToString(); label7.Text = "7 x " + b + " = " + (b * 7).ToString(); label9.Text = "8 x " + b + " = " + (b * 8).ToString(); label8.Text = "9 x " + b + " = " + (b * 9).ToString(); label10.Text = "10 x " + b + " = " + (b * 10).ToString(); label11.Text = "11 x " + b + " = " + (b * 11).ToString(); label12.Text = "12 x " + b + " = " + (b * 12).ToString(); }
+    private void Form1_Load(object sender, EventArgs e) { } } }
